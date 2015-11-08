94fbe4f80a76a71fa262f811ad7a67de842d6c3a
Preetham Gujjula <preetham.gujjula@gmail.com>
2015-11-07 17:01:38 -0800

    Refactored Hilbert.Modular

--- src/Hilbert/Modular.hs
+++ src/Hilbert/Modular.hs
@@ -1,16 +1,17 @@
 module Hilbert.Modular where
 
--- Computes base^exp (mod modulus)
--- Precondition: modulus > 0, exp >= 0, base >= 0
+-- Computes b^e (mod m)
+-- Precondition: e >= 0, m > 0
 -- modPow 0 0 _ = 1
 modPow :: (Integral a) => a -> a -> a -> a
-modPow base exp modulus | exp == 0 = 1
-modPow base exp modulus | exp == 1 = base `rem` modulus
-modPow base exp modulus | abs base >= modulus
-  = modPow (base `rem` modulus) exp modulus
-modPow base exp modulus | exp `rem` 2 == 0
-  = ((modPow base redExp modulus)^2) `rem` modulus
-    where redExp = exp `div` 2
-modPow base exp modulus
-  = (base * (modPow base redExp modulus)^2) `rem` modulus
-     where redExp = (exp - 1) `div` 2
+modPow b e m
+-- Corner case
+  | e == 0                = 1
+-- Ensure 0 <= b < m
+  | abs b >= m || b < 0   = modPow (b `mod` m) e m
+-- Base case
+  | e == 1                = b
+-- Recursive cases
+  | e `rem` 2 == 0        = (modPow b e' m)^2 `rem` m
+  | otherwise             = (b * (modPow b e' m)^2) `rem` m
+                                where e' = e `div` 2
