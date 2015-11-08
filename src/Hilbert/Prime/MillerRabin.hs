108c2ba617de2ae0c0a6aa4e701aad446189948c
Preetham Gujjula <pgujjula+gitcommit@protonmail.com>
2015-11-08 10:31:47 -0800

    Created test for Miller Rabin

--- src/Hilbert/Prime/MillerRabin.hs
+++ src/Hilbert/Prime/MillerRabin.hs
@@ -5,7 +5,7 @@ import Hilbert.Modular (modPow)
 -- Miller-Rabin probabilistic prime test
 -- Precondition: n >= 3
 millerRabin :: (Integral a) => a -> [a] -> Bool
-millerRabin n testBases = not $ all (baseTest n) testBases
+millerRabin n testBases = not $ any (baseTest n) testBases
   where baseTest n a = (part1 n a) && (part2 n a)
         part1 n a = (modPow a d n) /= 1
         part2 n a = all (\r -> (modPow a (2^r * d) n) /= (n - 1))
