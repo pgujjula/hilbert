c6ddc5637e482f4368619c27ddf7684efd9871a7
Preetham Gujjula <preetham.gujjula@gmail.com>
2015-11-08 01:10:44 -0800

    Removed CReal.hs, because it was written by someone else and idk about the licensing.

--- src/Hilbert/List.hs
+++ src/Hilbert/List.hs
@@ -1,6 +1,6 @@
 module Hilbert.List (rmDups) where
 
--- Removes duplicates from a sortedList
+-- Removes duplicates from a sorted list
 rmDups :: (Eq a) => [a] -> [a]
 rmDups (x1:x2:xs) = if x1 == x2
                     then rmDups (x1:xs)
