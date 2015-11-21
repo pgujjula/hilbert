942997e16e28dd35c33f075b35821797b7401a32
Preetham Gujjula <pgujjula+gitcommit@protonmail.com>
2015-11-20 18:06:50 -0800

    Created tests for Hilbert.ContinuedFrac and used a better algorithm for continuedFrac

--- src/Hilbert/ContinuedFrac.hs
+++ src/Hilbert/ContinuedFrac.hs
@@ -34,24 +34,23 @@ minus (Irrational a b c d) n = Irrational (newa `div` g)
         newd = d
         g = gcd (gcd newa newc) newd
 
-
 convergent :: (Integral a) => [a] -> a -> Ratio a
 convergent _ 0 = 0
 convergent (x:xs) n = 1 / ((fromIntegral x) + (convergent xs (n - 1)))
 
-continuedFrac n | isSquare n = Nothing
-continuedFrac n = Just $ ((map fst $ take first cfrac),
-               (map fst $ drop first $ take second cfrac))
-  where cfrac = continuedFrac' n
-        repeat = fromJust $ findRepeat cfrac
-        (first:second:_) = findIndices (== repeat) cfrac
-        continuedFrac' x = generate (a0, b0)
+continuedFrac :: (Integral a) => a -> Maybe (a, [a])
+continuedFrac x | isSquare x = Nothing
+continuedFrac x = Just $ (first, periodic)
+  where untruncated = continuedFracUntruncated x
+        first = head untruncated
+        rest = tail untruncated
+        periodic = takeUntil (== (2*first)) rest
+
+        takeUntil proc (x1:xs) = if (proc x1)
+                                    then [x1]
+                                    else x1:(takeUntil proc xs)
+        takeUntil _ xs = xs
+        continuedFracUntruncated x = map fst $ generate (a0, b0)
           where a0 = floor $ sqrt $ fromIntegral x
                 b0 = reciprocal (Irrational 1 x (-a0) 1)
                 generate (a, b) = (a, b):(generate (flr b, reciprocal(b `minus` flr(b))))
-        findRepeat list = findRepeat' [] list
-          where findRepeat' old (n:new) = 
-                  case find (== n) old of
-                    Nothing -> findRepeat' (n:old) new
-                    otherwise -> Just n
-
