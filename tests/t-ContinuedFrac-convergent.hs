f2030a7f6a60b098886965ee0ccd65c15c3fb068
Preetham Gujjula <preetham.gujjula@gmail.com>
2015-12-03 14:26:29 -0800

    Added documentation to ContinuedFrac tests

--- tests/t-ContinuedFrac-convergent.hs
+++ tests/t-ContinuedFrac-convergent.hs
@@ -1,3 +1,5 @@
+-- Test ContinuedFrac.convergent by ensuring that 
+-- convergents for a square root actually approach a square root.
 module Main where
 
 import System.Exit (exitFailure, exitSuccess)
@@ -8,10 +10,13 @@ import Data.List (sort)
 import Hilbert.ContinuedFrac (convergent, cfracSqrt)
 import Hilbert.Square (isSquare)
 
+-- Compute the infinite continued fraction expansion
+-- for sqrt n
 toInfinite :: (Integral a) => a -> [a]
 toInfinite n = a:(cycle b)
   where Just (a, b) = cfracSqrt n
 
+-- Check that convergents approach the target square root
 test n = (reverse $ sort approximations) == approximations
  where
   list = [1..limit]
@@ -21,10 +26,11 @@ test n = (reverse $ sort approximations) == approximations
   approximations = map (abs . ((sqrt (fromIntegral n)) - ) . fromRational
                           . (convergent cfrac)) list
 
-
+-- Generate test cases for all nonsquares less than 1000.
 cases :: [Integer]
-cases = filter (not . isSquare) [1..10]
+cases = filter (not . isSquare) [1..1000]
 
+-- The failed test cases
 wrongs :: [Integer]
 wrongs = filter (not . test) cases
 
