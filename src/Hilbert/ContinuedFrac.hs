8fd41c8b2966ce96714483872fef5cd6c802b1cf
Preetham Gujjula <preetham.gujjula@gmail.com>
2015-12-02 16:29:01 -0800

    Added more documentation to ContinuedFrac + renamed continuedFrac -> cfracSqrt

--- src/Hilbert/ContinuedFrac.hs
+++ src/Hilbert/ContinuedFrac.hs
@@ -1,6 +1,7 @@
+{- | Functions for handling continued fractions. -}
 module Hilbert.ContinuedFrac
   ( convergent
-  , continuedFrac) where
+  , cfracSqrt) where
 
 import Data.Maybe (fromJust)
 import Data.List (find, findIndices)
@@ -36,44 +37,50 @@ minus (Irrational a b c d) n = Irrational (newa `div` g)
         newd = d
         g = gcd (gcd newa newc) newd
 
--- |/convergent xs n/ computes the nth convergent of the continued
---  fraction represented by xs
---
--- >>> convergent [1, 2, 2, 2..] 1
--- 1 % 1
--- >>> convergent [1, 2, 2, 2..] 3
--- 7 % 5
+{- |@convergent xs n@ computes the nth convergent of the continued
+    fraction represented by @xs@
+   
+   For example, to compute convergents of /e/, whose continued fraction
+   expansion is @[2, 1, 2, 1, 1, 4, 1, 1, 6, 1, 1, 8, 1...]@
+   
+   >>> let cfrac = (2:) $ concat $ map (\x -> [1, x, 1]) [2, 4..]
+   >>> convergent cfrac 10
+   1457 % 536
+   >>> (fromRational $ convergent cfrac 21) == (exp 1)
+   True
+-}
+
 convergent :: (Integral a) => [a] -> a -> Ratio a
 convergent _ 0 = 0
 convergent (x:xs) 1 = fromIntegral x
 convergent (x:xs) n = (fromIntegral x) + (1/(convergent xs (n - 1)))
 
-{- |@continuedFrac x@ returns @'Nothing'@ if @x@ is a square. Otherwise, it
+{- |@cfracSqrt x@ returns @'Nothing'@ if @x@ is a square. Otherwise, it
   returns @'Just' (n, xs)@, which represents the continued fraction representation
   of @sqrt x@.
 
     [@n@] is the non-repeating first term of the continued fraction,
     [@xs@] represents a single cycle of the repeating part of the continued fraction.
 
-  >>> continuedFrac 41
+  >>> cfracSqrt 41
   Just (6, [2, 2, 12])
-  >>> continuedFrac 4
+  >>> cfracSqrt 4
   Nothing
 
   For example, to compute a good approximation for @sqrt 2@
 
   @
   approx = fromRational $ convergent (n:(repeat xs)) 30
-           where Just (n, xs) = continuedFrac 2
+           where Just (n, xs) = cfracSqrt 2
   >>> approx - (sqrt 2)
   0.0
   @
 -}
 
-continuedFrac :: (Integral a) => a -> Maybe (a, [a])
-continuedFrac x | isSquare x = Nothing
-continuedFrac x = Just $ (first, periodic)
-  where untruncated = continuedFracUntruncated x
+cfracSqrt :: (Integral a) => a -> Maybe (a, [a])
+cfracSqrt x | isSquare x = Nothing
+cfracSqrt x = Just $ (first, periodic)
+  where untruncated = cfracSqrtUntruncated x
         first = head untruncated
         rest = tail untruncated
         periodic = takeUntil (== (2*first)) rest
@@ -82,7 +89,7 @@ continuedFrac x = Just $ (first, periodic)
                                     then [x1]
                                     else x1:(takeUntil proc xs)
         takeUntil _ xs = xs
-        continuedFracUntruncated x = map fst $ generate (a0, b0)
+        cfracSqrtUntruncated x = map fst $ generate (a0, b0)
           where a0 = floor $ sqrt $ fromIntegral x
                 b0 = reciprocal (Irrational 1 x (-a0) 1)
                 generate (a, b) = (a, b):(generate (flr b, reciprocal(b `minus` flr(b))))
