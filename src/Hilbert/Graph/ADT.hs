4d18c0253515d91f966028e1eb91510c010d95e9
Preetham Gujjula <preetham.gujjula@gmail.com>
2015-12-07 21:58:24 -0800

    Added more to Graph.EdgeList

--- src/Hilbert/Graph/ADT.hs
+++ src/Hilbert/Graph/ADT.hs
@@ -6,6 +6,7 @@ module Hilbert.Graph.ADT where
 -}
 data GraphType = Undirected
                | Directed
+      deriving (Eq, Show)
 
 {- | The graph typeclass. Implementations are provided in 
      other modules. A good default implementation is provided in
@@ -19,45 +20,54 @@ class GraphADT g where
   emptyDirected :: g v e 
 
   -- | Returns true if a graph is empty.
-  null :: g e v -> Bool
+  null :: g v e -> Bool
+
+  -- | Get the type of a graph (Undirected/Directed)
+  graphType :: g v e -> GraphType
 
   -- | Returns the number of vertices in the graph.
-  numVertices :: g e v -> Int
+  numVertices :: (Ord v) => g v e -> Int
 
   -- | Returns the number of edges in the graph.
-  numEdges :: g e v -> Int
+  numEdges :: g v e -> Int
 
   {- | Inserts a vertex with no neighbors into the graph.
        Does nothing if the vertex already exists.
   -}
-  insertVertex :: (Ord v) => v -> g e v -> g e v
+  insertVertex :: (Ord v) => v -> g v e -> g v e
 
   {- | Inserts an edge between two vertices. An existing edge
-       between the two vertices will be replaced.
+       between the two vertices will be replaced. If one or both
+       of the vertices does not exist, then they will be added.
   -}
-  insertEdge :: (Ord e) => v -> v -> e -> g e v -> g e v
+  insertEdge :: (Ord v, Ord e) => v -> v -> e -> g v e -> g v e
 
   {- | Deletes a vertex from the graph. Does nothing if the
        vertex does not exist. Deletes all associated edges as well.
   -}
-  deleteVertex :: (Ord v) => v -> g e v -> g e v
+  deleteVertex :: (Ord v) => v -> g v e -> g v e
 
   {- | Deletes an edge from the graph. Does nothing if the 
        edge does not exist.
   -}
-  deleteEdge :: (Ord v, Num v) => v -> v -> g e v -> g e v
+  deleteEdge :: (Ord v, Num v) => v -> v -> g v e -> g v e
 
-  -- | Finds the edge weight between two connected vertices.
-  weight :: (Ord v) => v -> v -> g e v -> Maybe e
+  {- | Finds the edge weight between two connected vertices.
+       Returns Nothing if the vertices are not connected.
+  -}
+  weight :: (Ord v) => v -> v -> g v e -> Maybe e
 
   -- | Returns whether the two vertices are connected.
-  isNeighbor :: (Ord v) => v -> v -> g e v -> Bool
+  isNeighbor :: (Ord v) => v -> v -> g v e -> Bool
 
   -- | Returns a list of neighbors of the vertex.
-  neighbors :: (Ord v) => v -> g e v -> [v]
+  neighbors :: (Ord v) => v -> g v e -> [v]
 
   -- | Convert a list to a graph.
   fromList :: (Ord v, Ord e) => GraphType -> [(v, v, e)] -> g v e
 
-  -- | Convert a graph to a list.
-  toList :: g v e -> (GraphType, [(v, v, e)])
+  -- | Get the vertices of a graph
+  vertices :: (Ord v) => g v e -> [v]
+
+  -- | Get the edges of a graph
+  edges :: (Ord v) => g v e -> [(v, v, e)]
--- src/Hilbert/Graph/EdgeList.hs
+++ src/Hilbert/Graph/EdgeList.hs
@@ -5,30 +5,67 @@
 -}
 module Hilbert.Graph.EdgeList where
 
+import Data.List (sort)
+
 import Hilbert.Graph.ADT
+import Hilbert.List (rmDups)
 
 data EdgeList v e = EdgeList {
-                      graphType :: GraphType,
-                      list      :: [(v, v, e)]
+                      gtype :: GraphType, -- ^so as not to conflict with ADT.graphType
+                      vs  :: [v],
+                      es :: [(v, v, e)]
                     } 
+        deriving (Show, Eq)
+
+swap :: (v, v, e) -> (v, v, e)
+swap (v1, v2, e) = (v2, v1, e)
+
+compareSwap :: (Ord v) => (v, v, e) -> (v, v, e)
+compareSwap (v1, v2, e) = if v2 < v1
+                          then (v2, v1, e)
+                          else (v1, v2, e)
+
+cmp (v1, _, _) (v2, _, _) = compare v1 v2
+sameVertex (v1, _, _) (v2, _, _) = v1 == v2
 
 instance GraphADT EdgeList where
-  emptyUndirected = EdgeList Undirected []
-  emptyDirected =   EdgeList Directed []
+  emptyUndirected = EdgeList Undirected [] []
+  emptyDirected   = EdgeList Directed [] []
 
-  null = Prelude.null . list
+  null = Prelude.null . vs
 
-  numVertices = undefined
+  graphType = gtype
 
-  numEdges = length . list
+  numVertices = length . vs
 
-  insertVertex = undefined
-  insertEdge = undefined
+  numEdges = length . es
+
+  insertVertex v graph@(EdgeList gtype vs es) =
+      if v `elem` vs
+      then graph
+      else EdgeList gtype (v:vs) es
+
+  insertEdge v1 v2 e graph@(EdgeList Directed _ _) = graphWithEdge
+    where graphWithVertices = insertVertex v1 (insertVertex v2 graph)
+          graphWithEdge = if (v1, v2, e) `elem` (es graphWithVertices)
+                          then graphWithVertices
+                          else EdgeList Directed (vs graphWithVertices)
+                                            ((v1, v2, e):(es graphWithVertices))
+
+  insertEdge v1 v2 e graph@(EdgeList Undirected _ _) = graphWithEdge
+    where graphWithVertices = insertVertex v1 (insertVertex v2 graph)
+          graphWithEdge = if (v1, v2, e) `elem` (es graphWithVertices)
+                           || (v2, v1, e) `elem` (es graphWithVertices)
+                          then graphWithVertices
+                          else EdgeList Undirected (vs graphWithVertices)
+                                          ((v1, v2, e):(es graphWithVertices))
+
   deleteVertex = undefined
   deleteEdge = undefined
   weight = undefined
   isNeighbor = undefined
   neighbors vertex graph = undefined
 
-  fromList = EdgeList
-  toList g = (graphType g, list g)
+  fromList = undefined
+  vertices = vs
+  edges = es
