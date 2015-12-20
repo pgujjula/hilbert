02609586613f19faa112120114955431ebc92d6d
Preetham Gujjula <preetham.gujjula@gmail.com>
2015-12-19 21:50:33 -0800

    IDK what this is committing

--- tests/t-Graph-EdgeList-emptyUndirected.hs
+++ tests/t-Graph-EdgeList-emptyUndirected.hs
@@ -3,39 +3,17 @@ module Main where
 import System.Exit (exitFailure, exitSuccess)
 import Hilbert.Graph.ADT as ADT
 import Hilbert.Graph.EdgeList
+import Hilbert.Test
 import Data.Maybe
 
 {- Test that emptyUndirected returns an empty graph. -}
-test1 :: Maybe String
-test1 = if actual == expected
-        then Nothing
-        else Just $ "In computing: null emptyUndirected\n"
-          ++ "  expected: " ++ (show expected) ++ "\n"
-          ++ "  but got: " ++ (show actual) ++ "\n"
-    where actual = ADT.null (emptyUndirected::EdgeList Int Int)
-          expected = True
+test1 = Single "null emptyUndirected::(EdgeList Int Int)"
+               (show True)
+               (show $ ADT.null (emptyUndirected::(EdgeList Int Int)))
 
 {- Test that emptyUndirected returns an Undirected graph. -}
-test2 :: Maybe String
-test2 =  if actual == expected
-         then Nothing
-         else Just $ "In computing: graphType emptyUndirected\n"
-          ++ "expected: " ++ (show expected) ++ "\n"
-          ++ "but got : " ++ (show actual)   ++ "\n"
-    where actual = graphType (emptyUndirected::EdgeList Int Int)
-          expected = Undirected
+test2 = Single "graphType emptyUndirected::(EdgeList Int Int)"
+               (show Undirected)
+               (show $ graphType (emptyUndirected::(EdgeList Int Int)))
 
-{- Chain tests together into a new test. -}
-chain :: [Maybe String] -> Maybe String
-chain tests = if Prelude.null errors
-        then Nothing
-        else Just (concat errors)
-    where errors = map fromJust $ filter isJust tests
-
-{- Run a test. -}
-runTest Nothing = exitSuccess
-runTest (Just err) = do
-                  putStrLn err
-                  exitFailure
-
-main = runTest $ chain [test1, test2]
+main = mruntest (Chain [test1, test2])
