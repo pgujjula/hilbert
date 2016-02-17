e057baae8c1cbc4ea893cbf7fc51e4225c3b9d33
Preetham Gujjula <pgujjula@berkeley.edu>
2016-02-17 01:08:53 -0800

    Deleted a lot of files to start from the beginning.

--- src/Hilbert/Prime/BailliePSW.hs
+++
@@ -1,9 +0,0 @@
-module Hilbert.Prime.BailliePSW where
-
-import Hilbert.Prime.Lucas (lucas)
-import Hilbert.Prime.MillerRabin (millerRabin)
-
--- Baillie-PSW primality test
--- Precondition: n >= 3
-bailliePSW :: (Integral a) => a -> Bool
-bailliePSW n = (millerRabin n [2]) && (lucas n)
