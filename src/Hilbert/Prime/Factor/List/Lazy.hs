44bf0b1bc007c08f8e35b014c220bf89ce66c8f4
Preetham Gujjula <pgujjula+gitcommit@protonmail.com>
2016-06-29 23:29:40 -0700

    Refactored Prime.List.Lazy

--- src/Hilbert/Prime/Factor/List/Lazy.hs
+++ src/Hilbert/Prime/Factor/List/Lazy.hs
@@ -13,74 +13,42 @@ module Hilbert.Prime.Factor.List.Lazy
   , factorToInf
   ) where
 
+import Control.Monad.State.Lazy
+import Control.Applicative ((<$>))
+import Data.List (mapAccumL)
+import Data.Tuple (swap)
+
 import Hilbert.PriorityQueue
+import Hilbert.Prime.List.Lazy (primes)
 import Hilbert.Square (integralSqrt)
-import Data.List (foldl')
+
+type PQueue = DefaultQueue
 
 {-| 
     Factor the first @n@ positive integers lazily. Lower memory usage and faster
     than @take n factorizations@.
 -}
 factorTo :: Int -> [[(Int, Int)]]
-factorTo n = zipWith clean [1..] $ [[], [(2, 1)]] ++ (stepAllLazy limit n (insert (2, tail (powers 2)) 4 em) 3)
-  where em = empty :: MapQueue (Int, [Int]) Int
+factorTo n = (prefix ++) $ snd $ mapAccumL step' startQueue [3..n]
+  where step' a b = swap (step b a)
+        prefix = [[], [(2, 1)]]
+        startQueue = insert 2 4 empty
         limit = integralSqrt n
 
-insertPair :: (PriorityQueueADT q, Ord p) => q v p -> (v, p) -> q v p
-insertPair queue (v, p) = insert v p queue
-
--- Convert factorization to an integer
-multiplyOut :: [(Int, Int)] -> Int
-multiplyOut = product . (map (\(p, e) -> p^e))
-
-clean :: Int -> [(Int, Int)] -> [(Int, Int)]
-clean n fs = if leftover > 1
-             then fs ++ [(leftover, 1)]
-             else fs
-    where leftover = n `div` (multiplyOut fs)
-
-stepAllLazy :: (PriorityQueueADT q)
-        => Int
-        -> Int
-        -> q (Int, [Int]) Int
-        -> Int
-        -> [[(Int, Int)]]
-stepAllLazy limit maxN queue n | maxN < n = []
-stepAllLazy limit maxN queue n = f:fs
-  where (f, queue') = stepLazy limit queue n
-        fs = stepAllLazy limit maxN queue' (n + 1)
-
-stepLazy :: (PriorityQueueADT q)
-     => Int
-     -> q (Int, [Int]) Int
-     -> Int
-     -> ([(Int, Int)], q (Int, [Int]) Int)
-stepLazy limit queue n | n /= minP  = ([(n, 1)], newQueue)
-  where minP = snd $ peekMinP queue
-        newQueue = if n <= limit
-                   then insert (n, tail (powers n)) (2*n) queue
-                   else queue
-
-stepLazy limit queue n = (factorization, newQueue)
-  where minP = snd $ peekMinP queue
-        factorization :: [(Int, Int)]
-        factorization = map (\(p, powerList) -> (p, head powerList)) primePowerList
-        primePowerList :: [(Int, [Int])]
-        (primePowerList, queue') = deleteAllMin queue
-        reinsert :: [((Int, [Int]), Int)]
-        reinsert = map (\(p, powerList) -> ((p, tail powerList), n + p)) primePowerList
-        newQueue = foldl' insertPair queue' reinsert
-
-powers :: Int -> [Int]
-powers n = list
-  where list = altLeft (n - 1) (repeat 1) (map (+1) list)
-
-        altLeft :: Int -> [a] -> [a] -> [a]
-        altLeft n xs ys = prev ++ (altRight n rest ys)
-          where (prev, rest) = splitAt n xs
-
-        altRight :: Int -> [a] -> [a] -> [a]
-        altRight n xs (y:ys) = y:(altLeft n xs ys)
+        step :: Int -> PQueue Int Int -> ([(Int, Int)], PQueue Int Int)
+        step n = runState $ do
+            primality <- (n <) <$> (gets (snd . peekMinP))
+            if primality
+            then do
+              if n <= limit
+              then modify (insert n (n^2))
+              else return ()
+              return [(n, 1)]
+            else do
+              removedPrimes <- state deleteAllMin
+              forM_ removedPrimes $ \p ->
+                modify (insert p (n + p))
+              return (factorUsing removedPrimes n)
 
 {-|
     Get a lazy infinite list of factorizations of the poistive integers.
@@ -90,33 +58,38 @@ powers n = list
     [[], [(2, 1)], [(3, 1)], [(2, 2)], [(5, 1)]]
 -}
 factorToInf :: [[(Int, Int)]]
-factorToInf = [[], [(2, 1)]] ++ (stepAll (insert (2, tail (powers 2)) 4 em) 3)
-  where em = empty :: MapQueue (Int, [Int]) Int
+factorToInf = (prefix ++) $ snd $ mapAccumL step' startQueue [3..]
+  where step' a b = swap (step b a)
+        prefix = [[], [(2, 1)]]
+        startQueue = insert 2 4 empty
 
-stepAll :: (PriorityQueueADT q)
-        => q (Int, [Int]) Int
-        -> Int
-        -> [[(Int, Int)]]
-stepAll queue n = f:fs
-  where (f, queue') = step queue n
-        fs = stepAll queue' (n + 1)
+        step :: Int -> PQueue Int Int -> ([(Int, Int)], PQueue Int Int)
+        step n = runState $ do
+            primality <- (n <) <$> (gets (snd . peekMinP))
+            if primality
+            then do
+              modify (insert n (n^2))
+              return [(n, 1)]
+            else do
+              removedPrimes <- state deleteAllMin
+              forM_ removedPrimes $ \p ->
+                modify (insert p (n + p))
+              return (factorUsing removedPrimes n)
+
+factorUsing :: [Int] -> Int -> [(Int, Int)]
+factorUsing factors n = (zip factors exponents) ++ extra
+  where (leftover, exponents) = mapAccumL divideOut n factors
+        extra = if leftover == 1
+                then []
+                else [(leftover, 1)]
+
 {-
-   step queue computes the prime factorization of the minimum priority element 
-   in the queue, reinserts any removed prime powers, and returns the 
-   factorization and the new queue as a tuple.
+   divideOut n, k = (n', e), where
+     * k^e divides n, but k^(e + 1) does not (i.e., e = k || n)
+     * n' = n / (k^e)
 -}
-step :: (PriorityQueueADT q)
-     => q (Int, [Int]) Int
-     -> Int
-     -> ([(Int, Int)], q (Int, [Int]) Int)
-step queue n 
-  | n /= minP  = ([(n, 1)], insert (n, tail (powers n)) (2*n) queue)
-  | otherwise  = (factorization, newQueue)
-  where minP = snd $ peekMinP queue
-        factorization :: [(Int, Int)]
-        factorization = map (\(p, powerList) -> (p, head powerList)) primePowerList
-        primePowerList :: [(Int, [Int])]
-        (primePowerList, queue') = deleteAllMin queue
-        reinsert :: [((Int, [Int]), Int)]
-        reinsert = map (\(p, powerList) -> ((p, tail powerList), n + p)) primePowerList
-        newQueue = foldl' insertPair queue' reinsert
+divideOut :: Int -> Int -> (Int, Int)
+divideOut n k = if n `rem` k == 0
+                then let (n', f) = divideOut (n `quot` k) k
+                     in (n', f + 1)
+                else (n, 0)
