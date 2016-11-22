11cead250149a95f758832043ac6019e593ce85d
Preetham Gujjula <pgujjula@berkeley.edu>
2016-11-22 00:59:03 -0800

    Add jacobi test to Hilbert.Legendre
    Added testing for jacobi in Hilbert.Legendre. Checks the definition
    Jacobi symbol, defined in terms of the Legendre symbol.

--- test/Hilbert/LegendreSpec.hs
+++ test/Hilbert/LegendreSpec.hs
@@ -5,7 +5,7 @@ module Hilbert.LegendreSpec
 
 import Test.Hspec
 import Test.Hspec.QuickCheck
-import Test.QuickCheck (property, Gen, elements, forAll, conjoin, Property)
+import Test.QuickCheck (property, Gen, elements, forAll, conjoin, Property, choose)
 import Test.HUnit
 import Test.Hspec.Contrib.HUnit (fromHUnitTest)
 
@@ -14,9 +14,9 @@ import Data.List (sort)
 import qualified Data.Set as Set
 import Data.Function (on)
 
-import Hilbert.Legendre (legendre)
+import Hilbert.Legendre (legendre, jacobi)
 import Hilbert.List (rmDups)
-import Hilbert.Prime (primesTo)
+import Hilbert.Prime (primesTo, factor)
 
 main :: IO ()
 main = hspec spec
@@ -27,9 +27,9 @@ main = hspec spec
 -- The number of tests to conduct of each type
 numberOfTests = 500 :: Int
 
--- Upper bound for primes tested
-primeBound :: (Integral a) => a
-primeBound = 10000
+-- Upper bound for values in the tests
+bound :: (Integral a) => a
+bound = 10000
 
 {-
    Tests
@@ -40,12 +40,12 @@ spec = modifyMaxSuccess (\_ -> numberOfTests) $ do
             modifyMaxSuccess (`div` 5) testResidue
             modifyMaxSuccess (`div` 5) testNonResidue
          describe "jacobi" $ do
-           return ()
+            testAgainstLegendre
 
 testMultiple =
     it "legendre a p == 0 if a is a multiple of p" $ 
         usingTester testPrime
-    where testPrime :: Int -> Expectation
+    where testPrime :: Integer -> Expectation
           testPrime p = sequence_ $ zipWith shouldBe
                            (map (\n -> legendre n p) multiples)
                            (repeat 0)
@@ -55,7 +55,7 @@ testMultiple =
 testResidue = 
     it "legendre a p == -1 if a is a quadratic residue of p" $
         usingTester testPrime
-    where testPrime :: Int -> Expectation
+    where testPrime :: Integer -> Expectation
           testPrime p = sequence_ $ zipWith shouldBe
                                 (map (\n -> legendre n p) qrs)
                                 (repeat 1)
@@ -64,7 +64,7 @@ testResidue =
 testNonResidue = 
     it "legendre a p == 1 if a is a quadratic residue of p" $
         usingTester testPrime
-    where testPrime :: Int -> Expectation
+    where testPrime :: Integer -> Expectation
           testPrime p = sequence_ $ zipWith shouldBe
                                 (map (\n -> legendre n p) nonqrs)
                                 (repeat (-1))
@@ -72,11 +72,17 @@ testNonResidue =
                   nonqrs = Set.toList $ (Set.difference `on` Set.fromList)
                                          [1..p - 1] qrs
 
+
+testAgainstLegendre =  
+    it "jacobi a n == product (legendre a p_i)^(k_i), where n = product (p_i)^(k_i)" $ do
+        forAll pairGen $ \(a, n) ->
+           (jacobi a n) `shouldBe` (jacobiFromLegendre a n)
+
 {-
    Supplementary data/functions
 -}
-oddPrimeGen :: (Integral a) => Gen Int
-oddPrimeGen = elements $ drop 1 $ primesTo primeBound
+oddPrimeGen :: (Integral a) => Gen Integer
+oddPrimeGen = elements $ drop 1 $ map toInteger $ primesTo bound
 
 quadraticResidues :: (Integral a) => a -> [a]
 quadraticResidues p = drop 1    -- drop the 0
@@ -84,8 +90,14 @@ quadraticResidues p = drop 1    -- drop the 0
                     $ map (\x -> (x^2) `rem` p)
                         [0..p - 1]
 
+pairGen :: Gen (Integer, Integer)
+pairGen = do
+    a <- choose (-bound, bound)
+    n <- elements [1, 3..bound]
+    return (a, n)
+
 -- Apply a test on a single odd prime to many odd primes
-usingTester :: (Int -> Expectation) -> Property
+usingTester :: (Integer -> Expectation) -> Property
 usingTester testPrime = 
      conjoin -- Small cases
              [property (testPrime 3), 
@@ -93,3 +105,7 @@ usingTester testPrime =
               property (testPrime 7),
              -- General case
               forAll oddPrimeGen testPrime]
+
+jacobiFromLegendre :: Integer -> Integer -> Integer
+jacobiFromLegendre a n = product $ map (\(p, k) -> (legendre a' p)^k) (factor n)
+    where a' = a `mod` n
