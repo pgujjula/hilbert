2838227cb47aed7a80ad44bb31930d08c8b727fd
Preetham Gujjula <pgujjula+gitcommit@protonmail.com>
2019-06-08 14:31:29 -0500

    Added egcd and modInv to Hilbert.Modular

--- src/Hilbert/Modular.hs
+++ src/Hilbert/Modular.hs
@@ -11,6 +11,8 @@
 
 module Hilbert.Modular
     ( modPow
+    , egcd
+    , modInv
     ) where
 
 {-|
@@ -43,10 +45,74 @@ modPow_low_level a b m
   -- Corner case
   | b == 0                = 1
   -- Ensure 0 <= a < m
-  | a >= m                = modPow (a `rem` m) b m
+  | a >= m                = modPow_low_level (a `rem` m) b m
   -- Base case
   | b == 1                = a
   -- Recursive cases
-  | b `rem` 2 == 0        = (modPow a b' m)^2 `rem` m
-  | otherwise             = (a * (modPow a b' m)^2) `rem` m
+  | b `rem` 2 == 0        = (modPow_low_level a b' m)^2 `rem` m
+  | otherwise             = (a * (modPow_low_level a b' m)^2) `rem` m
                                 where b' = b `quot` 2
+
+{-|
+    @egcd m n@ is the extended GCD of @m@ and @n@. It is a tuple @(g, (a, b))@
+    such that @gcd m n == g@ and @a*m + b*n == g@.
+
+    __Preconditions:__
+
+      * None
+
+    >>> egcd 3 5
+    (1, (2, -1))
+    >>> egcd 10 4
+    (2, (1, -2))
+    >>> egcd (-4) 7
+    (1, (-2, -1))
+    >>> egcd 4 0
+    (4, (1, 0))
+    >>> egcd 0 0
+    (0, (0, 0))
+-}
+egcd :: (Integral a) => a -> a -> (a, (a, a))
+egcd m n
+  | m < n  = (g', (signa * a', signb * b'))
+  | otherwise = (g', (signb * b', signa * a'))
+  where (g', (a', b')) = egcd_low_level (abs lower) (abs upper)
+        signa = signum lower
+        signb = signum upper
+        lower = min m n
+        upper = max m n
+
+{- Here, we are guaranteed n > m > 0 -}
+egcd_low_level :: (Integral a) => a -> a -> (a, (a, a))
+egcd_low_level m n = egcd_recursive m n (1, 0) (0, 1)
+
+egcd_recursive :: (Integral a) => a -> a -> (a, a) -> (a, a) -> (a, (a, a))
+egcd_recursive m n m_coeffs n_coeffs
+  | m == 0 = (n, n_coeffs)
+  | otherwise = egcd_recursive r m (n0 - q*m0, n1 - q*m1) m_coeffs
+  where (q, r) = quotRem n m
+        (m0, m1) = m_coeffs
+        (n0, n1) = n_coeffs
+
+{-|
+    @modInv a m@ is the modular inverse of @a@ modulo @m@, if it exists.
+
+    __Preconditions:__
+
+      * None
+
+    >>> modInv 3 5
+    Just 2
+    >>> modInv 2 6
+    Nothing
+    >>> modInv 3 (-5)
+    Just 2
+    >>> modInv 16 7
+    Just 4
+    >>> modInv (-5) 1
+    Just 1
+-}
+modInv :: (Integral a) => a -> a -> Maybe a
+modInv a m = if g == 1 then Just b' else Nothing
+  where (g, (b, _)) = egcd a m
+        b' = if b > 0 then b else b + m
