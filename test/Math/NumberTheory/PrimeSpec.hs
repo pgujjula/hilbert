ed11968229c17734efc32b10c790bdbbbb9151ab
Preetham Gujjula <pgujjula+gitcommit@protonmail.com>
2020-05-04 19:33:56 -0500

    Add primes

---
+++ test/Math/NumberTheory/PrimeSpec.hs
@@ -0,0 +1,52 @@
+module Math.NumberTheory.PrimeSpec (spec) where
+
+import Test.Hspec (it, describe, Spec, shouldBe)
+import Test.QuickCheck (forAll, choose, (===))
+
+import Math.NumberTheory.Power (integralSqrt)
+import Math.NumberTheory.Prime (isPrime, primes, primesTo)
+
+spec :: Spec
+spec = do
+    describe "isPrime" isPrimeSpec
+    describe "primes" primesSpec
+    describe "primesTo" primesToSpec
+--    describe "factor" factorSpec
+--    describe "factorizations" factorizationsSpec
+--    describe "factorizationsTo" factorizationsToSpec
+
+isPrimeSpec :: Spec
+isPrimeSpec = do
+    it "negative numbers not prime" $ do
+        isPrime (-1) `shouldBe` False
+        isPrime (-2) `shouldBe` False
+    it "0 not prime" $ do
+        isPrime 0 `shouldBe` False
+    it "1 not prime" $ do
+        isPrime 1 `shouldBe` False
+
+    let naive n = n >= 2 && (not $ any (\d -> n `rem` d == 0) [2..integralSqrt n])
+    it "primes up to 1000 correct" $ do
+        filter isPrime [1..1000] `shouldBe` filter naive [1..1000]
+
+    it "works on arbitrary Ints" $
+        forAll (choose (1 :: Int, 100000)) $ \x -> isPrime x === naive x
+
+primesSpec :: Spec
+primesSpec =
+    it "correct for primes up to 10000" $ do
+        takeWhile (<= 10000) primes `shouldBe` (filter isPrime [1..10000])
+
+primesToSpec :: Spec
+primesToSpec =
+    it "correct for primes up to 10000" $ do
+        primesTo 10000 `shouldBe` (filter isPrime [1..10000])
+
+--factorSpec :: Spec
+--factorSpec = undefined :: Spec
+--
+--factorizationsSpec :: Spec
+--factorizationsSpec = undefined :: Spec
+--
+--factorizationsToSpec :: Spec
+--factorizationsToSpec = undefined :: Spec
