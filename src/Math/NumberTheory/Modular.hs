f0edf8a9d5880d010e9befbb5ab1a283c3b99ad3
Preetham Gujjula <pgujjula+gitcommit@protonmail.com>
2020-06-23 13:09:43 -0500

    Add chinese remainder

--- src/Math/NumberTheory/Modular.hs
+++ src/Math/NumberTheory/Modular.hs
@@ -8,14 +8,26 @@
 
     Functions to perform modular arithmetic.
 -}
+{-# LANGUAGE TupleSections #-}
 
 module Math.NumberTheory.Modular
     ( modPow
     , egcd
     , modInv
+    , chineseRemainder
+    , chineseRemainderF
     ) where
 
-import Math.NumberTheory.Power (square)
+import Control.Arrow                  ((>>>))
+import Control.Monad                  (foldM, (>=>))
+import Data.Maybe                     (fromJust)
+import Data.Ord (comparing)
+import Data.Foldable                  (foldl')
+import Data.List.Duplicate            (groupBy)
+
+import Math.NumberTheory.Power        (square)
+import Math.NumberTheory.Prime        (Prime, unPrime)
+import Math.NumberTheory.Prime.Factor (Factorization, factor)
 
 {-| @modPow a b m @ efficiently computes @mod (a^b) (abs m)@. 'error' is called
     when preconditions are not met.
@@ -24,7 +36,7 @@ import Math.NumberTheory.Power (square)
 
         * @b@ ≥ 0
         * @m@ ≠ 0
-    
+
     >>> modPow 3 5 10   -- 3^5 `mod` 10 == 243 `mod` 10 == 3
     3
 -}
@@ -105,3 +117,58 @@ modInv a m = if g == 1 then Just b' else Nothing
   where (g, (b, _)) = egcd a m'
         b' = if b > 0 then b else b + m'
         m' = abs m
+
+{-| Solve a system of modular congruences. The input list is a list of pairs
+    @(ai, mi)@, representing a system
+
+    \[\forall i \quad x \equiv a_{i} \mod m_{i} \]
+
+    The result @(x, m)@ is the solution @x@ and its modulus @m@, if a
+    solution exists.
+
+    >>> EXAMPLE HERE
+-}
+chineseRemainder :: Integral a => [(a, a)] -> Maybe (a, a)
+chineseRemainder = traverse (\(x, m) -> (x,) <$> factor (abs m))
+               >=> chineseRemainderF
+
+{-| Like 'chineseRemainder', but takes in the factorizations of the moduli to
+    speed up computation.
+
+    >>> EXAMPLE HERE
+-}
+chineseRemainderF :: Integral a => [(a, Factorization a)] -> Maybe (a, a)
+chineseRemainderF = breakUp >>> massage >>> fmap (uncurry solveAllPrime)
+                >>> sequence >>> fmap solveAll
+
+breakUp :: [(a, Factorization a)] -> [(a, (Prime a, Int))]
+breakUp = concatMap (\(x, fact) -> fmap (x,) fact)
+
+massage :: Ord b => [(a, (b, c))] -> [(b, [(a, c)])]
+massage = fmap (\(a, (b, c)) -> (b, (a, c)))
+      >>> groupBy (comparing fst)
+      >>> fmap (\xs -> (fst (head xs), fmap snd xs))
+
+solveAll :: Integral a => [(a, a)] -> (a, a)
+solveAll = foldl' solve (0, 1)
+
+solve :: Integral a => (a, a) -> (a, a) -> (a, a)
+solve (a, m) (b, n) = (x, s)
+  where
+    s = m*n
+    x = ((a * (fromJust $ modInv n m)) * n
+       + (b * (fromJust $ modInv m n)) * m)
+       `rem` s
+
+-- Given a prime p and a list of (xi, ki) representing a system of congruences
+--    x == xi (mod p^ki)
+-- solve for x, k such that x (mod p^k) is a solution to the system.
+solveAllPrime :: Integral a => Prime a -> [(a, Int)] -> Maybe (a, a)
+solveAllPrime p = fmap (\(x, k) -> (x, unPrime p^k))
+                . foldM (solvePrime p) (0, 0)
+
+solvePrime :: Integral a => Prime a -> (a, Int) -> (a, Int) -> Maybe (a, Int)
+solvePrime p (x1, k1) (x2, k2)
+    | k1 > k2 = solvePrime p (x2, k2) (x1, k1)
+    | (x2 - x1) `mod` (unPrime p^k1) /= 0 = Nothing
+    | otherwise = Just (x2, k2)
