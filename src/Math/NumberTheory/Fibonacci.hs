c19386fde25a5e1863584afe98857775563fa3c3
Preetham Gujjula <gitcommit@mail.preetham.io>
2021-12-18 03:08:16 -0800

    fix fibonaccis module

--- src/Math/NumberTheory/Fibonacci.hs
+++ src/Math/NumberTheory/Fibonacci.hs
@@ -11,23 +11,17 @@
 module Math.NumberTheory.Fibonacci
     ( -- * Fibonacci numbers
       fibonaccis
-    , fibonaccisMod
     , fibonacciN
-    , fibonacciModN
 
     -- * Lucas numbers
     , lucasNums
-    , lucasNumsMod
     , lucasNumN
-    , lucasNumModN
 
     -- * Generalized Lucas sequences
     -- | See <https://en.wikipedia.org/wiki/Lucas_sequence Wikipedia> for a
     --   definition of /U/ and /V/.
     , lucasSeq
-    , lucasSeqMod
     , lucasSeqN
-    , lucasSeqModN
     ) where
 
 {-| The Fibonacci numbers.
@@ -35,68 +29,34 @@ module Math.NumberTheory.Fibonacci
     >>> take 10 fibonaccis
     [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 -}
-fibonaccis :: (Integral a) => [a]
+fibonaccis :: (Num a) => [a]
 fibonaccis = fst $ lucasSeq 1 (-1)
-
-{-| The Fibonacci numbers, modulo some base. Calls 'error' when the base is 0.
-
-    >>> take 10 (fibonaccisMod 10)
-    [0, 1, 1, 2, 3, 5, 8, 3, 1, 4]
--}
-fibonaccisMod :: (Integral a) => a -> [a]
-fibonaccisMod m = fst $ lucasSeqMod m 1 (-1)
 
 {-| The @n@th Fibonacci number. Calls 'error' when @n@ is less than 0.
 
     >>> map fibonacciN [0..9]
     [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 -}
-fibonacciN :: (Integral a, Integral b) => b -> a
+fibonacciN :: (Num a, Integral b) => b -> a
 fibonacciN = fst . lucasSeqN 1 (-1)
-
-{-| @fibonacciModN m n@ is the @n@th Fibonacci number, modulo @m@. Calls 'error'
-    when @m@ is zero or @n@ is less than 0.
-
-    >>> map (fibonacciModN 10) [0..9]
-    [0, 1, 1, 2, 3, 5, 8, 3, 1, 4]
--}
-fibonacciModN :: (Integral a, Integral b) => a -> b -> a
-fibonacciModN m = fst . lucasSeqModN m 1 (-1)
 
 {-| The Lucas numbers.
 
     >>> take 10 lucasNums
     [2, 1, 3, 4, 7, 11, 18, 29, 47, 76]
 -}
-lucasNums :: (Integral a) => [a]
+lucasNums :: (Num a) => [a]
 lucasNums = snd $ lucasSeq 1 (-1)
-
-{-| The Lucas numbers, modulo some base. Calls 'error' when the base is 0.
-
-    >>> take 10 (lucasNumsMod 10)
-    [2, 1, 3, 4, 7, 1, 8, 9, 7, 6]
--}
-lucasNumsMod :: (Integral a) => a -> [a]
-lucasNumsMod m = snd $ lucasSeqMod m 1 (-1)
 
 {-| The @n@th Lucas number. Calls 'error' when @n@ is less than 0.
 
     >>> map lucasN [0..9]
     [2, 1, 3, 4, 7, 11, 18, 29, 47, 76]
 -}
-lucasNumN :: (Integral a, Integral b) => b -> a
+lucasNumN :: (Num a, Integral b) => b -> a
 lucasNumN = snd . lucasSeqN 1 (-1)
 
-{-| @lucasModN m n@ is the @n@th Lucas number, modulo @m@. Calls 'error'
-    when @m@ is zero or @n@ is less than 0.
-
-    >>> map (lucasNumModN 10) [0..9]
-    [2, 1, 3, 4, 7, 1, 8, 9, 7, 6]
--}
-lucasNumModN :: (Integral a, Integral b) => a -> b -> a
-lucasNumModN m = snd . lucasSeqModN m 1 (-1)
-
-go :: (Integral a) => (a -> a -> a) -> a -> a -> [a]
+go :: (Num a) => (a -> a -> a) -> a -> a -> [a]
 go f a b = a : seq c (go f b c)
   where
     c = f a b
@@ -110,46 +70,22 @@ go f a b = a : seq c (go f b c)
     >>> take 10 v
     [2, 1, 3, 4, 7, 11, 18, 29, 47, 76]
 -}
-lucasSeq :: (Integral a) => a -> a -> ([a], [a])
+lucasSeq :: (Num a) => a -> a -> ([a], [a])
 lucasSeq p q = (go f 0 1, go f 2 p)
   where
     f x y = -q*x + p*y
-
-{-| The /U/ and /V/ sequences associated with seeds /p/ and /q/, modulo a base.
-
-    >>> let modulus = 10
-    >>> let (p, q) = (1, -1)
-    >>> let (u, v) = lucasSeqMod modulus p q
-    >>> take 10 u
-    [0, 1, 1, 2, 3, 5, 8, 3, 1, 4]
-    >>> take 10 v
-    [2, 1, 3, 4, 7, 1, 8, 9, 7, 6]
--}
-lucasSeqMod :: (Integral a) => a -> a -> a -> ([a], [a])
-lucasSeqMod m p q = (go f u0 u1, go f v0 v1)
-  where
-    f x y = (q'*x + p'*y) `rem` m'
-    m' = abs m
-    p' = p `mod` m'
-    q' = (-q) `mod` m'
-
-    u0 = 0
-    u1 = 1 `rem` m'
-
-    v0 = 2 `rem` m'
-    v1 = p'
 
 -- Represent the nth and (n + 1)th Lucas U- and V- numbers
 data Group a = Group !a !a !a !a
   deriving (Eq, Show, Ord)
 
-step :: (Integral a) => (a -> a) -> a -> a -> Group a -> Group a
+step :: (Num a) => (a -> a) -> a -> a -> Group a -> Group a
 step reduce p q (Group un un1 vn vn1) = Group un1 un2 vn1 vn2
   where
     un2 = reduce $ (-q)*un + p*un1
     vn2 = reduce $ (-q)*vn + p*vn1
 
-double :: Integral a => (a -> a) -> (a -> b -> a) -> a -> a -> b -> Group a -> Group a
+double :: Num a => (a -> a) -> (a -> b -> a) -> a -> a -> b -> Group a -> Group a
 double reduce pow p q n (Group un un1 vn vn1) = Group u2n u2n1 v2n v2n1
   where
     u2n  = reduce $ un * vn
@@ -169,7 +105,7 @@ double reduce pow p q n (Group un un1 vn vn1) = Group u2n u2n1 v2n v2n1
     >>> v
     76
 -}
-lucasSeqN :: (Integral a, Integral b) => a -> a -> b -> (a, a)
+lucasSeqN :: (Num a, Integral b) => a -> a -> b -> (a, a)
 lucasSeqN p q = (\(Group u _ v _) -> (u, v)) . mkGroup
   where
     mkGroup n
@@ -178,35 +114,3 @@ lucasSeqN p q = (\(Group u _ v _) -> (u, v)) . mkGroup
         | otherwise = double id (^) p q halfN (mkGroup halfN)
       where
         halfN = n `div` 2
-
-{-| The @n@th terms of the /U/ and /V/ sequences associated with seeds /p/ and
-    /q/, modulo some base
-    
-    >>> let modulus = 10
-    >>> let (p, q) = (1, -1)
-    >>> let n = 9
-    >>> let (u, v) = lucasSeqN modulus p q n
-    >>> u
-    4
-    >>> v
-    6
--}
-lucasSeqModN :: (Integral a, Integral b) => a -> a -> a -> b -> (a, a)
-lucasSeqModN m p q = (\(Group u _ v _) -> (u, v)) . mkGroup
-  where
-    mkGroup n
-        | n == 0    = Group u0 u1 v0 v1
-        | odd n     = step reduce p' q' (mkGroup (n - 1))
-        | otherwise = double reduce (^) p' q' halfN (mkGroup halfN)
-      where
-        halfN = n `div` 2
-
-    m' = abs m
-    reduce = (`mod` m')
-    p' = reduce p
-    q' = reduce q
-
-    u0 = 0
-    u1 = reduce 1
-    v0 = reduce 2
-    v1 = p'
--- test/Math/NumberTheory/FibonacciSpec.hs
+++ test/Math/NumberTheory/FibonacciSpec.hs
@@ -1,42 +1,47 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE RankNTypes        #-}
-{-# LANGUAGE TypeApplications  #-}
+{-# OPTIONS_GHC -Wno-orphans    #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE FlexibleInstances   #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications    #-}
 module Math.NumberTheory.FibonacciSpec (spec) where
 
-import Data.Proxy (Proxy (Proxy), asProxyTypeOf)
+import Data.Mod                    (Mod, unMod)
+import Data.Proxy                  (Proxy (Proxy), asProxyTypeOf)
+import Math.NumberTheory.Fibonacci (fibonacciN, fibonaccis, lucasNumN,
+                                    lucasNums, lucasSeq, lucasSeqN)
 import System.Random               (Random)
+import System.Random.Stateful      (Uniform, UniformRange, uniformM, uniformRM,
+                                    uniformWord32R)
 import Test.Hspec                  (Expectation, Spec, describe, it, shouldBe)
-import Test.QuickCheck             (Gen, Property, choose, forAll, suchThat,
-                                   (.&&.), oneof)
+import Test.QuickCheck             (Gen, Property, choose, forAll, (.&&.))
 
-import Math.NumberTheory.Fibonacci (fibonacciModN, fibonacciN, fibonaccis,
-                                    fibonaccisMod, lucasNumModN, lucasNumN,
-                                    lucasNums, lucasNumsMod, lucasSeq,
-                                    lucasSeqMod, lucasSeqModN, lucasSeqN)
+instance Uniform (Mod 5) where
+  uniformM g = do
+    n <- uniformWord32R 4 g
+    pure (fromIntegral n)
+
+instance UniformRange (Mod 5) where
+  uniformRM (l, u) g = do
+    n <- uniformRM (unMod l, unMod u) g
+    pure (fromIntegral n)
+
+instance Random (Mod 5)
 
 spec :: Spec
 spec = do
     describe "fibonaccis"    fibonaccisSpec
-    describe "fibonaccisMod" fibonaccisModSpec
     describe "fibonacciN"    fibonacciNSpec
-    describe "fibonacciModN" fibonacciModNSpec
 
     describe "lucasNums"     lucasNumsSpec
-    describe "lucasNumsMod"  lucasNumsModSpec
     describe "lucasNumN"     lucasNumNSpec
-    describe "lucasNumModN"  lucasNumModNSpec
 
     describe "lucasSeq"      lucasSeqSpec
-    describe "lucasSeqMod"   lucasSeqModSpec
     describe "lucasSeqN"     lucasSeqNSpec
-    describe "lucasSeqModN"  lucasSeqModNSpec
 
 -- parameters
 listLimit :: Integral a => a
 listLimit = 10^(2 :: Int)
-
-modLimit :: Int
-modLimit = 10^(2 :: Int)
 
 seedLimit :: Int
 seedLimit = 30
@@ -60,175 +65,76 @@ instance (PrefixComparable a) => PrefixComparable (a, a) where
 transpose :: [(a, a)] -> ([a], [a])
 transpose xys = (map fst xys, map snd xys)
 
-seedsGen :: (Integral a, Random a) => Gen (a, a)
+seedsGen :: (Num a, Random a) => Gen (a, a)
 seedsGen = (,) <$> seedGen <*> seedGen
   where
     seedGen = choose (-fromIntegral seedLimit, fromIntegral seedLimit)
 
-modGen :: (Integral a, Random a) => Gen a
-modGen = oneof [pure 1, choose (-modLimit', modLimit') `suchThat` (/= 0)]
+naiveLucas :: forall a. Num a => a -> a -> ([a], [a])
+naiveLucas p q = (us, vs)
   where
-    modLimit' = fromIntegral modLimit
-
-seedsModGen :: (Integral a, Random a) => Gen ((a, a), a)
-seedsModGen = (,) <$> seedsGen <*> modGen
-
-naiveLucas :: Integral a => Maybe a -> a -> a -> ([a], [a])
-naiveLucas modulus p q = (map fromIntegral us, map fromIntegral vs)
-  where
-    us :: [Integer]
-    us = reduce $ 0 : 1 : zipWith (-) (map (*p') (tail us)) (map (*q') us)
-    vs :: [Integer]
-    vs = reduce $ 2 : p' : zipWith (-) (map (*p') (tail vs)) (map (*q') vs)
-
-    p' :: Integer
-    p' = toInteger p
-
-    q' :: Integer
-    q' = toInteger q
-
-    reduce :: [Integer] -> [Integer]
-    reduce = case modulus of
-                 Nothing -> id
-                 Just m  -> map (`mod` abs (toInteger m))
-
---checkModular :: (Show a, Integral a) => a -> (a -> [a]) -> [a] -> Property
---checkModular m f ref =
---    take listLimit (f m) === take listLimit (map (`mod` m) ref)
+    us :: [a]
+    us = 0 : 1 : zipWith (-) (map (*p) (tail us)) (map (*q) us)
+    vs :: [a]
+    vs = 2 : p : zipWith (-) (map (*p) (tail vs)) (map (*q) vs)
 
 -- test first listLimit against reference
 fibonaccisSpec :: Spec
 fibonaccisSpec = it ("first " ++ show listLimit ++ " correct") $
     let naiveFibonaccis :: [Integer]
-        naiveFibonaccis = fst $ naiveLucas Nothing 1 (-1)
+        naiveFibonaccis = fst $ naiveLucas 1 (-1)
      in comparePrefix' fibonaccis naiveFibonaccis
-
--- test first listLimit against reference, for random modulus
--- test that modulus = 0 throws error
---
--- do above for @Int and @Integer
-fibonaccisModSpec :: Spec
-fibonaccisModSpec = it "correct for arbitrary modulus" $ 
-    let test :: (Integral a, Random a, Show a) => Proxy (Gen a) -> Property
-        test proxy = forAll (modGen `asProxyTypeOf` proxy) $ \m ->
-            let naiveFibonaccis = fst $ naiveLucas (Just m) 1 (-1)
-             in comparePrefix' (fibonaccisMod m) naiveFibonaccis
-     in test (Proxy @(Gen Int)) .&&. test (Proxy @(Gen Integer))
 
 -- test first listLimit against reference
 fibonacciNSpec :: Spec
 fibonacciNSpec = it ("first " ++ show listLimit ++ " correct") $
     let naiveFibonaccis :: [Integer]
-        naiveFibonaccis = fst $ naiveLucas Nothing 1 (-1)
+        naiveFibonaccis = fst $ naiveLucas 1 (-1)
      in do
         comparePrefix' (map fibonacciN [(0 :: Int)..]) naiveFibonaccis
         comparePrefix' (map fibonacciN [(0 :: Integer)..]) naiveFibonaccis
-
--- test first listLimit against reference, for random modulus
--- test that modulus = 0 throws error
---
--- do above for @Int and @Integer
-fibonacciModNSpec :: Spec
-fibonacciModNSpec = it "correct for arbitrary modulus" $ 
-    let test :: (Integral a, Random a, Show a) => Proxy (Gen a) -> Property
-        test proxy = forAll (modGen `asProxyTypeOf` proxy) $ \m ->
-            let naiveFibonaccis = fst $ naiveLucas (Just m) 1 (-1)
-             in do
-                comparePrefix' (map (fibonacciModN m) [(0 :: Int)..]) naiveFibonaccis
-                comparePrefix' (map (fibonacciModN m) [(0 :: Integer)..]) naiveFibonaccis
-     in test (Proxy @(Gen Int)) .&&. test (Proxy @(Gen Integer))
 
 -- test first listLimit against reference
 lucasNumsSpec :: Spec
 lucasNumsSpec = it ("first " ++ show listLimit ++ " correct") $
     let naiveLucasNums :: [Integer]
-        naiveLucasNums = snd $ naiveLucas Nothing 1 (-1)
+        naiveLucasNums = snd $ naiveLucas 1 (-1)
      in comparePrefix' lucasNums naiveLucasNums
-
--- test first listLimit against reference, for random modulus
--- test that modulus = 0 throws error
---
--- do above for @Int and @Integer
-lucasNumsModSpec :: Spec
-lucasNumsModSpec = it "correct for arbitrary modulus" $ 
-    let test :: (Integral a, Random a, Show a) => Proxy (Gen a) -> Property
-        test proxy = forAll (modGen `asProxyTypeOf` proxy) $ \m ->
-            let naiveLucasNums = snd $ naiveLucas (Just m) 1 (-1)
-             in comparePrefix' (lucasNumsMod m) naiveLucasNums
-     in test (Proxy @(Gen Int)) .&&. test (Proxy @(Gen Integer))
 
 -- test first listLimit against reference
 lucasNumNSpec :: Spec
 lucasNumNSpec = it ("first " ++ show listLimit ++ " correct") $
     let naiveLucasNums :: [Integer]
-        naiveLucasNums = snd $ naiveLucas Nothing 1 (-1)
+        naiveLucasNums = snd $ naiveLucas 1 (-1)
      in do
         comparePrefix' (map lucasNumN [(0 :: Int)..]) naiveLucasNums
         comparePrefix' (map lucasNumN [(0 :: Integer)..]) naiveLucasNums
 
--- test first listLimit against reference, for random modulus
--- test that modulus = 0 throws error
---
--- do above for @Int and @Integer
-lucasNumModNSpec :: Spec
-lucasNumModNSpec = it "correct for arbitrary modulus" $
-    let test :: (Integral a, Random a, Show a) => Proxy (Gen a) -> Property
-        test proxy = forAll (modGen `asProxyTypeOf` proxy) $ \m ->
-            let naiveLucasNums = snd $ naiveLucas (Just m) 1 (-1)
-             in do
-                comparePrefix' (map (lucasNumModN m) [(0 :: Int)..]) naiveLucasNums
-                comparePrefix' (map (lucasNumModN m) [(0 :: Integer)..]) naiveLucasNums
-     in test (Proxy @(Gen Int)) .&&. test (Proxy @(Gen Integer))
-
 -- check for random p and random q
 -- check Int and Integer
 lucasSeqSpec :: Spec
-lucasSeqSpec = it "correct for arbitrary seeds" $ 
-    let test :: (Integral a, Random a, Show a)
+lucasSeqSpec = it "correct for arbitrary seeds" $
+    let test :: (Num a, Random a, Show a, Ord a)
              => Proxy (Gen (a, a))
              -> Property
         test proxy =
             forAll (seedsGen `asProxyTypeOf` proxy) $ \(p, q) ->
-                comparePrefix' (lucasSeq p q) (naiveLucas Nothing p q)
+                comparePrefix' (lucasSeq p q) (naiveLucas p q)
      in ((test @Int) Proxy) .&&. ((test @Integer) Proxy)
-
-lucasSeqModSpec :: Spec
-lucasSeqModSpec = it "correct for arbitrary modulus and seeds" $ 
-    let test :: (Integral a, Random a, Show a)
-             => Proxy (Gen ((a, a), a))
-             -> Property
-        test proxy =
-            forAll (seedsModGen `asProxyTypeOf` proxy) $ \((p, q), m) ->
-                comparePrefix' (lucasSeqMod m p q) (naiveLucas (Just m) p q)
-     in ((test @Int) Proxy) .&&. ((test @Integer) Proxy)
+                            .&&. ((test @(Mod 5) Proxy))
 
 -- check for random p and random q, first listLimit
 -- check Int and Integer
 lucasSeqNSpec :: Spec
-lucasSeqNSpec = it "correct for arbitrary seeds" $ 
-    let test :: (Integral a, Random a, Show a)
+lucasSeqNSpec = it "correct for arbitrary seeds" $
+    let test :: (Num a, Random a, Show a, Ord a)
              => Proxy (Gen (a, a))
              -> Property
         test proxy =
             forAll (seedsGen `asProxyTypeOf` proxy) $ \(p, q) -> do
                 comparePrefix' (transpose $ map (lucasSeqN p q) [(0 :: Int)..])
-                               (naiveLucas Nothing p q)
+                               (naiveLucas p q)
                 comparePrefix' (transpose $ map (lucasSeqN p q) [(0 :: Integer)..])
-                               (naiveLucas Nothing p q)
+                               (naiveLucas p q)
      in ((test @Int) Proxy) .&&. ((test @Integer) Proxy)
-
--- check for random p and random q and random modulus
--- for first listLimit
--- check Int and Integer
-lucasSeqModNSpec :: Spec
-lucasSeqModNSpec = it "correct for arbitrary modulus and seeds" $ 
-    let test :: (Integral a, Random a, Show a)
-             => Proxy (Gen ((a, a), a))
-             -> Property
-        test proxy =
-            forAll (seedsModGen `asProxyTypeOf` proxy) $ \((p, q), m) -> do
-                comparePrefix' (transpose $ map (lucasSeqModN m p q) [(0 :: Int)..])
-                               (naiveLucas (Just m) p q)
-                comparePrefix' (transpose $ map (lucasSeqModN m p q) [(0 :: Integer)..])
-                               (naiveLucas (Just m) p q)
-     in ((test @Int) Proxy) .&&. ((test @Integer) Proxy)
+                            .&&. ((test @(Mod 5) Proxy))
