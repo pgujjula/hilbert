bed2bc08027308b4ff23b5a2d926abf2c78b85e5
Preetham Gujjula <gitcommit@mail.preetham.io>
2023-01-12 13:54:45 -0800

    Add script to load tests into ghci

---
+++ load_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+stack ghci \
+  --ghci-options -isrc \
+  --ghci-options -itest \
+  --ghci-options -itest \
+  --ghci-options -Wno-missing-home-modules \
+  --ghci-options -Wno-unused-packages \
+  --ghci-options -Wno-unused-packages \
+  --ghci-options "-package hspec" \
+  --ghci-options "-package QuickCheck" \
+  --ghci-options "-package tasty" \
+  --ghci-options "-package tasty-hspec" \
+  --ghci-options "-package HUnit" \
+  --ghci-options "-package random" \
+  --ghci-options "-Wno-type-defaults" \
+  --main-is hilbert:test:spec
