3d11c326e1c0541f6b4b234f7aa927f2fc6c58f5
Preetham Gujjula <gitcommit@mail.preetham.io>
2024-04-02 18:09:26 -0700

    Add multiplicativeOrder

--- src/Math/NumberTheory/Modular.hs
+++ src/Math/NumberTheory/Modular.hs
@@ -10,18 +10,22 @@ module Math.NumberTheory.Modular
   ( egcd,
     chineseRemainder,
     chineseRemainderF,
+    multiplicativeOrder,
+    multiplicativeOrderF,
   )
 where
 
 import Control.Arrow ((>>>))
-import Control.Monad (foldM)
+import Control.Monad (foldM, zipWithM)
 import Data.Foldable (foldl')
+import Data.List (elemIndex)
 import Data.List.Duplicate (groupBy)
 import Data.Maybe (fromJust)
-import Data.Mod (Mod, invertMod, unMod)
+import Data.Mod (Mod, invertMod, unMod, (^%))
 import Data.Ord (comparing)
 import Data.Type.Natural (SNat, withSNat)
-import Math.NumberTheory.Prime.Factor (Factorization, factor)
+import Math.NumberTheory.Divisor (relativelyPrime, totient)
+import Math.NumberTheory.Prime.Factor (Factorization, factor, simplify)
 
 -- | @egcd m n@ is the extended GCD of @m@ and @n@. It is a tuple @(g, (a, b))@
 --   such that @gcd m n == g@ and @a*m + b*n == g@.
@@ -69,14 +73,14 @@ egcdRecursive m n m_coeffs n_coeffs
 --   solution exists.
 --
 --   >>> TODO: EXAMPLE HERE
-chineseRemainder :: Integral a => [(a, a)] -> Maybe (a, a)
+chineseRemainder :: (Integral a) => [(a, a)] -> Maybe (a, a)
 chineseRemainder = chineseRemainderF . fmap (\(x, m) -> (x, factor (abs m)))
 
 -- | Like 'chineseRemainder', but takes in the factorizations of the moduli to
 --   speed up computation.
 --
 --   >>> EXAMPLE HERE
-chineseRemainderF :: Integral a => [(a, Factorization a)] -> Maybe (a, a)
+chineseRemainderF :: (Integral a) => [(a, Factorization a)] -> Maybe (a, a)
 chineseRemainderF =
   breakUp
     >>> massage
@@ -87,16 +91,16 @@ chineseRemainderF =
 breakUp :: [(a, Factorization a)] -> [(a, (a, Int))]
 breakUp = concatMap (\(x, fact) -> fmap (x,) fact)
 
-massage :: Ord b => [(a, (b, c))] -> [(b, [(a, c)])]
+massage :: (Ord b) => [(a, (b, c))] -> [(b, [(a, c)])]
 massage =
   fmap (\(a, (b, c)) -> (b, (a, c)))
     >>> groupBy (comparing fst)
     >>> fmap (\xs -> (fst (head xs), fmap snd xs))
 
-solveAll :: Integral a => [(a, a)] -> (a, a)
+solveAll :: (Integral a) => [(a, a)] -> (a, a)
 solveAll = foldl' solve (0, 1)
 
-solve :: Integral a => (a, a) -> (a, a) -> (a, a)
+solve :: (Integral a) => (a, a) -> (a, a) -> (a, a)
 solve (a, m) (b, n) = (x, s)
   where
     s = m * n
@@ -106,20 +110,48 @@ solve (a, m) (b, n) = (x, s)
       )
         `rem` s
 
-modInv :: Integral a => a -> a -> Maybe a
+modInv :: (Integral a) => a -> a -> Maybe a
 modInv a m = withSNat (fromIntegral m) $ \(_ :: SNat n) ->
   fmap (fromIntegral . unMod) (invertMod (fromIntegral a :: Mod n))
 
 -- Given a prime p and a list of (xi, ki) representing a system of congruences
 --    x == xi (mod p^ki)
 -- solve for x, k such that x (mod p^k) is a solution to the system.
-solveAllPrime :: Integral a => a -> [(a, Int)] -> Maybe (a, a)
+solveAllPrime :: (Integral a) => a -> [(a, Int)] -> Maybe (a, a)
 solveAllPrime p =
   fmap (\(x, k) -> (x, p ^ k))
     . foldM (solvePrime p) (0, 0)
 
-solvePrime :: Integral a => a -> (a, Int) -> (a, Int) -> Maybe (a, Int)
+solvePrime :: (Integral a) => a -> (a, Int) -> (a, Int) -> Maybe (a, Int)
 solvePrime p (x1, k1) (x2, k2)
   | k1 > k2 = solvePrime p (x2, k2) (x1, k1)
   | (x2 - x1) `mod` (p ^ k1) /= 0 = Nothing
   | otherwise = Just (x2, k2)
+
+-- | `multiplicativeOrder` @n@ @a@ is the multiplicative order of @a@ modulo
+-- @n@.
+multiplicativeOrder :: (Integral a) => a -> a -> Maybe a
+multiplicativeOrder n = multiplicativeOrderF n (factor (totient n))
+
+-- | `multiplicativeOrderF` @n@ @fact@ @a@ is the multiplicative order of @a@
+-- modulo @n@. Takes the factorization of
+-- `Math.NumberTheory.Prime.Factor.totient` @n@ to speed up the computation.
+multiplicativeOrderF :: forall a. (Integral a) => a -> Factorization a -> a -> Maybe a
+multiplicativeOrderF n fact a = withSNat (fromIntegral (abs n)) $ \(_ :: SNat n) ->
+  let a' :: Mod n
+      a' = fromIntegral a
+
+      t :: a
+      t = simplify fact
+
+      xs :: [(a, Mod n)]
+      xs = map ((\x -> (x, a' ^% x)) . (\(p, e) -> t `quot` (p ^ e))) fact
+
+      ys :: [Maybe Int]
+      ys = zipWith f xs fact
+        where
+          f :: (a, Mod n) -> (a, Int) -> Maybe Int
+          f (_, res) (p, _) = elemIndex 1 $ map unMod $ iterate (^% p) res
+   in if relativelyPrime n a
+        then simplify <$> zipWithM (\x y -> (x,) <$> y) (map fst fact) ys
+        else Nothing
